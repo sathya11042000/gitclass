f7187e4f93fb4f126bfbcfdee3c76248dd64d3b8
sathya11042000 <sathyastudy2000@gmail.com>
2025-01-29 16:16:31 +0530

    Employee added

--- src/org/level/Session.java
+++ src/org/level/Session.java
@@ -5,7 +5,12 @@ public class Session {
 	public static void main(String[] args) {
 
 		System.out.println("success");
+		int a = 10;
+		System.out.println(a);
+		
 
 	}
 
 }
+//git fetch -- 
+//git checkout
